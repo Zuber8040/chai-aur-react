d04b71789a419625259bbd93f6f6603dd848d5ef
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2024-08-15 18:41:35 +0530

    react redux tool kit project

---
+++ reduxToolKitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Addtodo from './components/Addtodo'
+import Todo from './components/Todo'
+
+function App() {
+ 
+  
+  return (
+    <>
+     <h1>Learn About Redu Tool Kit</h1>
+     <Addtodo/>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
